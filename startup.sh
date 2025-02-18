d3634a03b890c9126bd79e311d5b39a9f39edfb8
amanfreecs <aman.hobo@gmail.com>
2025-02-18 13:28:33 +0530

    add nvm

--- startup.sh
+++ startup.sh
@@ -8,4 +8,8 @@ nvm install node
 
 cd ./app && npm i
 
-npm run index.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm run start
