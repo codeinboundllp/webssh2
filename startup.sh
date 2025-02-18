81552e4d0081d546f5c15932b9f1fac2bc7b12c9
amanfreecs <aman.hobo@gmail.com>
2025-02-18 13:29:56 +0530

    proper script

--- startup.sh
+++ startup.sh
@@ -4,12 +4,12 @@
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-nvm install node
-
-cd ./app && npm i
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+nvm install node
+
+cd ./app && npm i
 
 npm run start
