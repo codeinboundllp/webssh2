25c510af14dab3e60a4ebc53c14ff552480fb4fc
amanfreecs <aman.hobo@gmail.com>
2025-07-18 20:58:40 +0530

    comment out nvm

--- startup.sh
+++ startup.sh
@@ -2,15 +2,15 @@
 
 #Script needs to be run from inside webssh2 folder#
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-nvm install node
+# nvm install node
 
-nvm use node
+# nvm use node
 
 cd ./app && npm i && npm run build
 
